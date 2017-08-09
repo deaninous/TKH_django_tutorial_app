f2ba541de55fab1d01f1110963fe4cf274e52d25
deaninous <ouedinous20@gmail.com>
2017-08-09 06:54:13 -0400

    Cookie session

--- tutorial_project/tutorial_app/views.py
+++ tutorial_project/tutorial_app/views.py
@@ -5,16 +5,54 @@ from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponse
-
+from datetime import datetime
 
 # Create your views here.
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
-	context_dict = {'categories' : category_list, 'pages' : page_list }
-	return render(request, 'index.html', context_dict)
+	context_dict = {'categories': category_list, 'pages': page_list}
+	visits = request.session.get('visits')
+	if not visits:
+		visits = 1
+		reset_last_visit_time = False
+	last_visit = request.session.get('last_visit')
+	if last_visit:
+		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+		if (datetime.now() - last_visit_time).seconds > 0:
+			visits = visits + 1
+			reset_last_visit_time = True
+	else:
+		reset_last_visit_time = True
+	if reset_last_visit_time:
+		request.session['last_visit'] = str(datetime.now())
+		request.session['visits'] = visits
+	context_dict['visits'] = visits
+	response = render(request,'index.html', context_dict)
+	return response
+
+	# def index(request):
+	# 	request.session.set_test_cookie()
+	# 	if request.session.test_cookie_worked():
+	# 		print ">>>> TEST COOKIE WORKED!"
+	# 		request.session.delete_test_cookie()
+	# 		category_list = Category.objects.order_by('-likes')[:5]
+	# 	page_list = Page.objects.order_by('-views')[:5]
+	# 	context_dict = {'categories' : category_list, 'pages' : page_list }
+	# 	return render(request, 'index.html', context_dict)
 def about(request):
-	return HttpResponse('about')
+	if request.session.get('visits'):
+		count = request.session.get('visits')
+	else:
+		count = 0
+	count = count + 1
+	try:
+		if context_dict:
+			pass
+	except:
+			context_dict = {}
+	context_dict['visits'] = count
+	return render(request, 'about.html', context_dict)
 def category(request, category_name_slug):
 	try:
 
